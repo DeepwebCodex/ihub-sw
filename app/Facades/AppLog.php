3e692dafb13dfcf80420ac5228cfad64eb1dfc1e
Тытарь Владимир <v.tytar@favbet.com>
2016-10-13 17:57:17 +0300

    App log Facade phpdoc annotations for IDE auto complete

--- app/Facades/AppLog.php
+++ app/Facades/AppLog.php
@@ -7,6 +7,15 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class AppLog
  * @package App\Facades
+ *
+ * @method static string alert(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string critical(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string error(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string warning(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string notice(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string info(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string debug(string $message, string $node = '', string $module = '', string $line = '')
+ * @method static string log(string $level,string $message, string $node = '', string $module = '', string $line = '')
  */
 class AppLog extends Facade
 {
