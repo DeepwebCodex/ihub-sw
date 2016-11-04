e7be95e85d9e976d7d96384fa9e02ddbeb7f657c
y.fedoruk <y.fedoruk@favbet.com>
2016-11-04 12:31:49 +0200

    egt. separate api test data

---
+++ tests/_support/EuroGamesTech/TestUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EuroGamesTech;
+
+use \App\Components\Users\IntegrationUser;
+
+class TestUser
+{
+    public function getUser()
+    {
+        return IntegrationUser::get(env('TEST_USER_ID'), 0, 'tests');
+    }
+
+    public function getBalanceInCents()
+    {
+        return IntegrationUser::get(env('TEST_USER_ID'), 0, 'tests')->getBalanceInCents();
+    }
+
+    public function getCurrency()
+    {
+        return IntegrationUser::get(env('TEST_USER_ID'), 0, 'tests')->getCurrency();
+    }
+}
