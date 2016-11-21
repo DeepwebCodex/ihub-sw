7fda4de629bf1ab1e8b14943ddb9919a9dd22702
psyhonut <psyhonut@gmail.com>
2016-11-21 18:45:05 +0200

    gitlab ci test

--- run/install-rancher.sh
+++ run/install-rancher.sh
@@ -5,13 +5,15 @@ chmod -R 777 ./storage && chmod -R 777 ./bootstrap/cache
 cp ./.env.example ./.env
 
 sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
+       -e "s/^APP_ENV=local/APP_ENV=local/g" \
        -e "s/^DB_HOST=127.0.0.1/DB_HOST=postgresql/g" \
        -e "s/^DB_USERNAME=pgsql/DB_USERNAME=postgres/g" \
        -e "s/^DB_PASSWORD=pgsq/DB_PASSWORD=mysecretpassword/g" \
        -e "s/^CACHE_DRIVER=file/CACHE_DRIVER=redis/g" \
        -e "s/^SESSION_DRIVER=file/SESSION_DRIVER=redis/g" \
        -e "s/^REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g" \
-       -e "s/^LOG_DRIVER=file/LOG_DRIVER=file/g" \
+       -e "s/^REDIS_PORT=6379/REDIS_PORT=6379/g" \
+       -e "s/^LOG_DRIVER=file/LOG_DRIVER=rabbit/g" \
        -e "s/^LOG_MONGO_SERVER=/LOG_MONGO_SERVER=mongodb:\/\/mongo:27017/g" \
        -e "s/^LOG_MONGO_DB_NAME=/LOG_MONGO_DB_NAME=logs/g" \
        -e "s/^LOG_MONGO_COLLECTION_NAME=/LOG_MONGO_COLLECTION_NAME=integration/g" \
