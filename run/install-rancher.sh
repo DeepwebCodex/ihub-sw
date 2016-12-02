70482d82bc62157ac82f5f62193abd2a4e8a1717
m.maksymenko <m.maksymenko@favbet.com>
2016-12-02 02:58:47 +0200

    change redis for tests

--- run/install-rancher.sh
+++ run/install-rancher.sh
@@ -11,8 +11,8 @@ sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
        -e "s/^DB_PASSWORD=pgsql/DB_PASSWORD=mysecretpassword/g" \
        -e "s/^CACHE_DRIVER=file/CACHE_DRIVER=redis/g" \
        -e "s/^SESSION_DRIVER=file/SESSION_DRIVER=redis/g" \
-       -e "s/^REDIS_HOST=127.0.0.1/REDIS_HOST=ihub.favbet.dev/g" \
-       -e "s/^REDIS_PORT=6379/REDIS_PORT=16379/g" \
+       -e "s/^REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g" \
+       -e "s/^REDIS_PORT=6379/REDIS_PORT=6379/g" \
        -e "s/^LOG_DRIVER=file/LOG_DRIVER=rabbit/g" \
        -e "s/^LOG_MONGO_SERVER=/LOG_MONGO_SERVER=mongodb:\/\/mongo:27017/g" \
        -e "s/^LOG_MONGO_DB_NAME=/LOG_MONGO_DB_NAME=logs/g" \
@@ -27,8 +27,10 @@ sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
 cp ./.env ./.env.testing
 
 sed -i -e "s/^APP_ENV=local/APP_ENV=testing/g" \
-       -e "s/^SESSION_DRIVER=redis/SESSION_DRIVER=file/g" \
-       -e "s/^LOG_DRIVER=rabbit/LOG_DRIVER=file/g" \
+       -e "s/^SESSION_DRIVER=redis/SESSION_DRIVER=redis/g" \
+       -e "s/^LOG_DRIVER=rabbit/LOG_DRIVER=rabbit/g" \
+       -e "s/^REDIS_HOST=redis/REDIS_HOST=ihub.favbet.dev/g" \
+       -e "s/^REDIS_PORT=6379/REDIS_PORT=16379/g" \
        ./.env.testing
 
 echo "" >> ./.env.testing
