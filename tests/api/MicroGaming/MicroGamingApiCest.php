ea2a8e316dd3b8a1f5f1def81c4f6640618b59da
Тытарь Владимир <v.tytar@favbet.com>
2016-12-15 10:45:49 +0200

    Fix tests response codes

--- tests/api/MicroGaming/MicroGamingApiCest.php
+++ tests/api/MicroGaming/MicroGamingApiCest.php
@@ -20,7 +20,7 @@ class MicroGamingApiCest
     public function testMethodNotFound(ApiTester $I)
     {
         $I->sendGET('/mg');
-        $I->seeResponseCodeIs(400);
+        $I->seeResponseCodeIs(200);
         $I->canSeeResponseIsXml();
         $I->expect('both items are in response');
         $I->seeXmlResponseIncludes(" <result seq=\"\" token=\"\" errorcode=\"6000\" errordescription=\"Empty source\"><extinfo/></result>");
