5e9897745c3f317d9eb73215c2c681e7ad1b1f5c
petroff <petroff@favbet.com>
2016-12-21 15:26:04 +0200

    start orion

---
+++ tests/unit/OrionResolverTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Codeception\Specify;
+use Codeception\Test\Unit;
+
+class OrionResolverTest extends Unit {
+
+    use Specify;
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before() {
+        
+    }
+
+    protected function _after() {
+        
+    }
+
+    // tests
+    public function test() {
+
+        $this->assertEquals(true, false);
+        
+        $this->specify("Test response formatter", function() {
+            
+        });
+    }
+
+}
