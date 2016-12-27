a75cf890a8a9ab331cc6fb5a788d446aa97b8213
m.maksymenko <m.maksymenko@favbet.com>
2016-12-27 11:14:10 +0200

    resolve conflict

--- run/dev/install-rancher.sh
+++ run/dev/install-rancher.sh
@@ -6,7 +6,6 @@ chmod -R 777 ./storage && chmod -R 777 ./bootstrap/cache
 cp ./.env.example ./.env
 sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
        -e "s/^APP_ENV=local/APP_ENV=dev/g" \
-       -e "s/^APP_REQUEST_DEBUG=false/APP_REQUEST_DEBUG=true/g" \
        -e "s/^DB_HOST=127.0.0.1/DB_HOST=de2db02d.dev.favorit/g" \
        -e "s/^DB_USERNAME=pgsql/DB_USERNAME=u_ihub/g" \
        -e "s/^DB_PASSWORD=pgsql/DB_PASSWORD=\"b9c3q46-9bv08967\"​/g" \
@@ -19,7 +18,7 @@ sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
        -e "s/^LOG_RABBIT_HOST=/LOG_RABBIT_HOST=rabbitmq-server/g" \
        -e "s/^LOG_RABBIT_PORT=/LOG_RABBIT_PORT=5672/g" \
        -e "s/^LOG_RABBIT_USER=/LOG_RABBIT_USER=ihub/g" \
-       -e "s/^LOG_RABBIT_PASS=/LOG_RABBIT_PASS=\"8jm7JMBmCsqp\"/g" \
+       -e "s/^LOG_RABBIT_PASS=/LOG_RABBIT_PASS=ihub/g" \
        -e "s/^STATSD_HOST=localhost/STATSD_HOST=statsd/g" \
        ./.env
 
@@ -32,9 +31,13 @@ sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
        -e "s/^DB_DATABASE=integrations/DB_DATABASE=\"ihub\"/g" \
        -e "s/^CACHE_DRIVER=file/CACHE_DRIVER=redis/g" \
        -e "s/^SESSION_DRIVER=file/SESSION_DRIVER=redis/g" \
-       -e "s/^REDIS_HOST=127.0.0.1/REDIS_HOST=de2red01d.dev.favorit/g" \
+       -e "s/^REDIS_HOST=127.0.0.1/REDIS_HOST=redis/g" \
        -e "s/^REDIS_PORT=6379/REDIS_PORT=6379/g" \
        -e "s/^LOG_DRIVER=file/LOG_DRIVER=file/g" \
+       -e "s/^LOG_RABBIT_HOST=/LOG_RABBIT_HOST=rabbitmq-server/g" \
+       -e "s/^LOG_RABBIT_PORT=/LOG_RABBIT_PORT=5672/g" \
+       -e "s/^LOG_RABBIT_USER=/LOG_RABBIT_USER=ihub/g" \
+       -e "s/^LOG_RABBIT_PASS=/LOG_RABBIT_PASS=ihub/g" \
        -e "s/^STATSD_HOST=localhost/STATSD_HOST=statsd/g" \
        ./.env.testing
 
