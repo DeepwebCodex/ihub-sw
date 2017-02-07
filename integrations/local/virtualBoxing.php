c4e3204e2156ef8b8da79f68838beff6eea99e4e
Pavel Kvach <p.kvach@favbet.com>
2017-02-07 18:11:36 +0200

    IHUB-86: Separate integration configs and routes

--- integrations/local/virtualBoxing.php
+++ integrations/local/virtualBoxing.php
@@ -2,39 +2,49 @@
 
 return [
     'service_id' => 23,
-    'sport_id' => 100,
+    'sport_id'  => 100,
     'amqp' => [
         'exchange' => 'service_23',
         'key' => 'service_23.event.'
     ],
     'country_id' => 267,
+    'enet_import' => 'undefined',
+    'import_odds_provider' => null,
+    'stop_loss' => 1000000.00,
     'gender' => 'mixed',
     'user_id' => 155,
+    'sport_union_id' => null,
+    'stop_loss_exp' => 1000000.00,
+    'max_bet_live' => null,
+    'max_payout_live' => null,
+    'info_url' => null,
     'rounds_map' => [1 => 31, 2 => 32, 3 => 33, 4 => 34, 5 => 35, 6 => 36],
     'scope_type' => ['point' => 1, 'knockdown' => 33, 'winner' => 12],
+    'type_participant' => 'athlete',
+    'status_type' => 'notstarted',
+    'event_type' => 'prebet',
+    'locked' => 'no',
+    'weigh' => 100,
+    'del' => 'no',
     'max_bet' => 10000.00,
     'max_payout' => 10000.00,
-    'stop_loss' => 1000000.00,
+    'margin' => 108.00,
+    'margin_prebet' => 108.00,
     'market' => [
-        'result_type_id' => 1
+        'OW' => 1,
+        'OW_result_type' => 1,
+        'max_bet' => 10000.00,
+        'max_payout' => 10000.00,
+        'stop_loss' => 1000000.00,
+        'OW_weight' => 100,
+        'match_result_type_id' => 1,
+        'CS' => 1318,
+        'CSR' => 1319,
+        'T65' => 50,
+        'OE' => 74,
+        'KO1' => 1314,
+        'KO2' => 1315,
+        'KO3' => 1316,
+        'KO4' => 1317,
     ],
-    'sports' => [
-        'box' => [
-            'max_bet' => 10000,
-            'max_payout' => 10000,
-            'stop_loss' => 1000000,
-            'game_result_scope_id' => '',
-            'markets' => [
-                'OW' => [1],
-                'CS' => [1318],
-                'CSR' => [1319],
-                'T65' => [50],
-                'OE' => [74],
-                'KO1' => [1314],
-                'KO2' => [1315],
-                'KO3' => [1316],
-                'KO4' => [1317]
-            ]
-        ]
-    ]
 ];
