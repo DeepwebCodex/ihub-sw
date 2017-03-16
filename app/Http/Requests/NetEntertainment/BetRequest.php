559a8cceb566840dc6e849168004a9a14e925f4d
m.maksymenko <m.maksymenko@favbet.com>
2017-03-17 00:25:07 +0200

    fix validation of user

--- app/Http/Requests/NetEntertainment/BetRequest.php
+++ app/Http/Requests/NetEntertainment/BetRequest.php
@@ -17,7 +17,7 @@ class BetRequest extends BaseRequest
     {
         return array_merge(parent::rules(), [
             'tid' => 'bail|required|min:1',
-            'userid' => 'bail|required|integer|min:1',
+            'userid' => 'bail|required|string|min:1',
             'currency' => 'bail|required|string|min:2',
             'amount' => 'bail|required|numeric|min:0.01',
         ]);
--- app/Http/Requests/NetEntertainment/WinRequest.php
+++ app/Http/Requests/NetEntertainment/WinRequest.php
@@ -17,7 +17,7 @@ class WinRequest extends BaseRequest
     {
         return array_merge(parent::rules(), [
             'tid' => 'bail|required|min:1',
-            'userid' => 'bail|required|integer|min:1',
+            'userid' => 'bail|required|string|min:1',
             'currency' => 'bail|required|string|min:2',
             'amount' => 'bail|required|min:1',
         ]);
