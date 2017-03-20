faace0523f8ccbf6841d559f7c9946db678b2fcb
m.maksymenko <m.maksymenko@favbet.com>
2017-03-20 15:35:21 +0200

    add playtech prod configs

--- integrations/production/DriveMediaPlaytech.php
+++ integrations/production/DriveMediaPlaytech.php
@@ -2,4 +2,96 @@
 
 return [
     'service_id' => 36,
+    '1813' => [
+        'currency' => 'UAH',
+        'key' => '9qS2E754t',
+    ],
+    '1814' => [
+        'currency' => 'RUB',
+        'key' => '46w8A03RN',
+    ],
+    '1816' => [
+        'currency' => 'USD',
+        'key' => 'oSJ753567',
+    ],
+    '1817' => [
+        'currency' => 'EUR',
+        'key' => 'B79p79Wn6',
+    ],
+    '1818' => [
+        'currency' => 'AUD',
+        'key' => '7Sh3497LA',
+    ],
+    '1819' => [
+        'currency' => 'BGN',
+        'key' => '5D6skE966',
+    ],
+    '1820' => [
+        'currency' => 'CAD',
+        'key' => 'D8486Cw8Q',
+    ],
+    '1821' => [
+        'currency' => 'DKK',
+        'key' => '0a8Qk733h',
+    ],
+    '1822' => [
+        'currency' => 'GBP',
+        'key' => 'i33l57B4L',
+    ],
+    '1823' => [
+        'currency' => 'HUF',
+        'key' => '2Waq8263V',
+    ],
+    '1824' => [
+        'currency' => 'INR',
+        'key' => 'ca6C0220r',
+    ],
+    '1825' => [
+        'currency' => 'JPY',
+        'key' => 'tBBd27279',
+    ],
+    '1826' => [
+        'currency' => 'MXN',
+        'key' => '36fJ170xE',
+    ],
+    '1827' => [
+        'currency' => 'NOK',
+        'key' => '4tQ18G648',
+    ],
+    '1828' => [
+        'currency' => 'PLN',
+        'key' => 'vJ5N1884P',
+    ],
+    '1829' => [
+        'currency' => 'RON',
+        'key' => 'WC5x5055x',
+    ],
+    '1830' => [
+        'currency' => 'SGD',
+        'key' => 'g32126RR6',
+    ],
+    '1831' => [
+        'currency' => 'SEK',
+        'key' => 'U8aZ98514',
+    ],
+    '1832' => [
+        'currency' => 'MYR',
+        'key' => 'x7J3750ws',
+    ],
+    '1833' => [
+        'currency' => 'MAD',
+        'key' => 'wS081457Q',
+    ],
+    '1834' => [
+        'currency' => 'TND',
+        'key' => 'i76F27yY7',
+    ],
+    '1835' => [
+        'currency' => 'CHF',
+        'key' => 'w2F29d90d',
+    ],
+    '1836' => [
+        'currency' => 'AMD',
+        'key' => '04Dn2878Q',
+    ],
 ];
