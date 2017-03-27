a35608cea858c2d5fd6b4f0a4603bb7141e177b3
m.maksymenko <m.maksymenko@favbet.com>
2017-03-27 19:12:41 +0300

    add dm nvmd prod configs

--- integrations/production/DriveMediaNovomaticDeluxe.php
+++ integrations/production/DriveMediaNovomaticDeluxe.php
@@ -3,9 +3,101 @@
 return [
     'service_id' => 39,
     'spaces' => [
-        '415' => [
+        '1808' => [
+            'currency' => 'FUN',
+            'key' => '29q7p41ic',
+        ],
+        '1860' => [
+            'currency' => 'UAH',
+            'key' => '066pQ72j2QZj',
+        ],
+        '1861' => [
+            'currency' => 'RUB',
+            'key' => 'P3h0m7c389Vl',
+        ],
+        '1862' => [
+            'currency' => 'USD',
+            'key' => '2zC73u242w0a',
+        ],
+        '1863' => [
             'currency' => 'EUR',
-            'key' => 'gfgyrf866',
+            'key' => '5VaV75c766S0',
+        ],
+        '1864' => [
+            'currency' => 'AUD',
+            'key' => 'EOt72611Rhz5',
+        ],
+        '1865' => [
+            'currency' => 'BGN',
+            'key' => 'f33j0K64Pyf9',
+        ],
+        '1866' => [
+            'currency' => 'CAD',
+            'key' => 'u1N77y871K0X',
+        ],
+        '1867' => [
+            'currency' => 'DKK',
+            'key' => 'Ma0M10Q31x6K',
+        ],
+        '1868' => [
+            'currency' => 'GBP',
+            'key' => 'SgX01080kl14',
+        ],
+        '1869' => [
+            'currency' => 'HUF',
+            'key' => 'guM935n05WEp',
+        ],
+        '1870' => [
+            'currency' => 'INR',
+            'key' => '3BdN708171o9',
+        ],
+        '1871' => [
+            'currency' => 'JPY',
+            'key' => 'S75HrCP4999d',
+        ],
+        '1872' => [
+            'currency' => 'MXN',
+            'key' => '2f3u8S874YZJ',
+        ],
+        '1873' => [
+            'currency' => 'NOK',
+            'key' => '3vir1G34LU7x',
+        ],
+        '1874' => [
+            'currency' => 'PLN',
+            'key' => 'pM24e1X6O7o2',
+        ],
+        '1875' => [
+            'currency' => 'RON',
+            'key' => 'c5GXh88V88eK',
+        ],
+        '1876' => [
+            'currency' => 'SGD',
+            'key' => '3ZG4p8x1h51g',
+        ],
+        '1877' => [
+            'currency' => 'SEK',
+            'key' => 'ziK2466210r2',
+        ],
+        '1878' => [
+            'currency' => 'MYR',
+            'key' => 'xfW05223zlUc',
+        ],
+        '1879' => [
+            'currency' => 'MAD',
+            'key' => 'UcOMb52382CW',
+        ],
+        '1880' => [
+            'currency' => 'TND',
+            'key' => 'AnP428d5093i',
+        ],
+        '1881' => [
+            'currency' => 'CHF',
+            'key' => '2K8uG15435lJ',
+        ],
+        '1882' => [
+            'currency' => 'AMD',
+            'key' => '1fHn41177KZk',
         ],
     ]
 ];
