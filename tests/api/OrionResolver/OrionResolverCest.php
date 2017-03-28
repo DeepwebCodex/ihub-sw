78a20fa833566f8f32545f0c1741f26ca03de65c
petroff <petroff@favbet.com>
2017-03-28 16:01:10 +0300

    added some test to orion

--- tests/api/OrionResolver/OrionResolverCest.php
+++ tests/api/OrionResolver/OrionResolverCest.php
@@ -3,7 +3,7 @@
 namespace api\OrionResolver;
 
 use ApiTester;
-use App\Components\ExternalServices\AccountManager;
+use App\Components\ExternalServices\MicroGaming\Orion\SoapEmulator;
 use Illuminate\Support\Facades\Artisan;
 use Mockery;
 
@@ -12,9 +12,9 @@ class OrionResolverCest
 
     private function initMock(ApiTester $I)
     {
-        $className = AccountManager::class;
+        $className = SoapEmulator::class;
         $mock = Mockery::mock($className);
-        $mock->shouldReceive('getData')->andReturn('<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><s:Fault
+        $mock->shouldReceive('sendRequest')->andReturn('<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><s:Fault
 ><faultcode xmlns:a="http://schemas.microsoft.com/ws/2005/05/addressing/none">a:
 ActionNotSupported</faultcode><faultstring xml:lang="en-US">The message with Act
 ion "http://mgsops.net/AdminAPI_Admin/IVanguardAdmin2/GetFailedEndGameQueue" can
@@ -24,6 +24,15 @@ ons between sender and receiver) or a binding/security mismatch between the send
 er and the receiver.  Check that sender and receiver have the same contract and
 the same binding (including security requirements, e.g. Message, Transport, None
 ).</faultstring></s:Fault></s:Body></s:Envelope>');
+        $I->getApplication()->instance($className, $mock);
+        $I->haveInstance($className, $mock);
+    }
+    
+    private function initMock2(ApiTester $I)
+    {
+        $className = SoapEmulator::class;
+        $mock = Mockery::mock($className);
+        $mock->shouldReceive('sendRequest')->andThrow(new \Exception('Ecxeption unknown'), 'Thrown exception');
         $I->getApplication()->instance($className, $mock);
         $I->haveInstance($className, $mock);
     }
@@ -54,7 +63,15 @@ the same binding (including security requirements, e.g. Message, Transport, None
         $this->initMock($I);
         Artisan::call('orion:endgame');
         $output = Artisan::output();
-        $I->assertEquals('Success.', trim($output));
+        $I->assertEquals('Something went wrong!', trim($output));
+    }
+    
+    public function testEndGameThrownException(ApiTester $I)
+    {
+        $this->initMock2($I);
+        Artisan::call('orion:endgame');
+        $output = Artisan::output();
+        $I->assertEquals('Something went wrong!', trim($output));
     }
 
 }
