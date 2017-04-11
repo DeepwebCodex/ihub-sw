790d59a8217ecab34dc9384e14a0251afbd06073
Pavel Kvach <p.kvach@favbet.com>
2017-04-11 17:09:16 +0300

    IHUB-140: Integrate Wirex Gaming (MILO) - trying add wsdl

---
+++ tests/api/WirexGaming/WirexGamingApiCest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class WirexGamingApiCest
+{
+    public function testMethodNotFound(\ApiTester $I)
+    {
+
+    }
+
+    public function testGetPersistentSession(\ApiTester $I)
+    {
+
+    }
+
+    public function testGetUserData(\ApiTester $I)
+    {
+
+    }
+
+    public function testGetAvailableBalance(\ApiTester $I)
+    {
+
+    }
+
+    public function testAddWithdrawEntry(\ApiTester $I)
+    {
+
+    }
+
+    public function testRollbackWithdraw(\ApiTester $I)
+    {
+
+    }
+
+    public function testAddDepositEntry(\ApiTester $I)
+    {
+
+    }
+
+    public function testCancelTransaction(\ApiTester $I)
+    {
+
+    }
+}
