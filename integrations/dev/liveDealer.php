5620dfb2acd798fc0624169472eb7c7a4093228c
y.fedoruk <y.fedoruk@favbet.com>
2017-04-25 15:06:03 +0300

    liveDealer config

--- integrations/dev/liveDealer.php
+++ integrations/dev/liveDealer.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'api_key' => "19d47072a7e46e82ce07b7d8b843cecc",
-    'secret_word' => "ererr4SD",
-    'system_1' => "998",
-    'api_password' => "4575885165224895",
-    'api_url' => "https://apitest.fundist.org/",
+    'secret_word' => "afangex3boov7Raipepe1roh4eequu6oojei7oofeephahnietho5aeGhee7aika",
     'service_id' => 21,
-    'shared_secret' => "Reiyozaa1eV6ahheeJauReihee7taet0LakohWo1raesaevooVa5shu4iegi8Abu",
 ];
