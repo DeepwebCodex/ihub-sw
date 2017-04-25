71704cc19c438784345d52dc2c30628ee064e06a
Андрей Петров <petroff@favbet.com>
2017-04-25 11:44:27 +0300

    fix sign novomatic deluxe

--- app/Components/Integrations/DriveMediaNovomaticDeluxe/Sign.php
+++ app/Components/Integrations/DriveMediaNovomaticDeluxe/Sign.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace App\Components\Integrations\DriveMediaNovomaticDeluxe;
 
@@ -15,20 +10,27 @@ use Illuminate\Support\Facades\Config;
  *
  * @author petroff
  */
-class Sign {
+class Sign
+{
 
     /**
      * @param array $data
      * @return string
      */
-    static public function generate(array $data): string {
+    static public function generate(array $data): string
+    {
         if (isset($data['sign'])) {
             unset($data['sign']);
         }
 
         $spaces = Config::get("integrations.DriveMediaNovomaticDeluxe.spaces");
         $currentSpace = $data['space'] ?? '';
-        $secretKey = array_get($spaces, $currentSpace . ".key");
+        foreach ($spaces as $v) {
+            if ($v['id'] === $currentSpace) {
+                $secretKey = $v['key'];
+                break;
+            }
+        }
         $str = http_build_query($data);
         $hash = md5($secretKey . $str);
 
