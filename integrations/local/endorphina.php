84e9a88993d2e9d0cafc0b4591bee55faec7c6da
Андрей Петров <petroff@favbet.com>
2017-04-25 15:53:34 +0300

    fix router and added local test credentinals for endorphina

--- integrations/local/endorphina.php
+++ integrations/local/endorphina.php
@@ -2,5 +2,5 @@
 
 return [
     'service_id' => 48,
-    'salt' => 'FavbetEGTSeamless',
+    'salt' => 'E09A0EF00E5D4B23B169E8548067B8E3',
 ];
--- routes/api/endorphina/routes.php
+++ routes/api/endorphina/routes.php
@@ -5,4 +5,5 @@ Route::get('balance', "EndorphinaController@balance");
 Route::post('bet', "EndorphinaController@bet");
 Route::post('win', "EndorphinaController@win");
 Route::post('refund', "EndorphinaController@refund");
+Route::any('/', "EndorphinaController@error");
 Route::any('{any}', "EndorphinaController@error");
