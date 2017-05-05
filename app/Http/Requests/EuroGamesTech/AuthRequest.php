f870a50eb5de869de5d009dad902741c7c22a9b3
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2017-05-05 12:52:26 +0300

    EGT fix Auth

--- app/Http/Requests/EuroGamesTech/AuthRequest.php
+++ app/Http/Requests/EuroGamesTech/AuthRequest.php
@@ -40,7 +40,7 @@ class AuthRequest extends BaseEgtRequest
             throw new ApiHttpException(400, "Defence code expired", ['code' => StatusCode::EXPIRED]);
         }
 
-        app('GameSession')->create(app('GameSession')->getData(), EgtHelper::SESSION_PREFIX . $request->input('SessionId'));
+        app('GameSession')->create(app('GameSession')->getData(), 'md5', EgtHelper::SESSION_PREFIX . $request->input('SessionId'));
 
         return parent::authorize($request);
     }
