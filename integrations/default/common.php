e053d813d8aab5a5f86b053f557d1cf2d3892b86
Maksymenko Mykola <m.maksymenko@favbet.com>
2017-06-22 14:53:12 +0300

    add proxy

---
+++ integrations/default/common.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'proxyExternalRequest' => [
+        'proxy' => [
+            'http' => 'http://10.120.12.56:3128/',
+            'https' => 'http://10.120.12.56:3128/',
+        ],
+    ]
+];
