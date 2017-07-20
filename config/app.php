05fb81c7b17cdfa1ec97cc87f5ac5311b00463f1
Maksymenko Mykola <m.maksymenko@betinvest.com>
2017-07-20 07:22:21 +0000

    Update app.php

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         \iHubGrid\ErrorHandler\Providers\GuzzleServiceProvider::class,
         \iHubGrid\DynamicScheduler\Providers\DynamicSchedulerServiceProvider::class,
         \iHubGrid\EuroGamesTech\EuroGamesTechServiceProvider::class,
-        \iHubGrid\BetGames\BetGamesServiceProvider::class
+        \iHubGrid\BetGames\BetGamesServiceProvider::class,
     ],
 
     /*
