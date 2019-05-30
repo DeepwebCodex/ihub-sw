733a84673f740aec99b288fec6a11cd21e6d6250
m.maksymenko <m.maksymenko@betinvest.com>
2019-05-30 10:44:43 +0300

    fixes

--- run/dev/make-configs-for-build.sh
+++ run/dev/make-configs-for-build.sh
@@ -22,15 +22,15 @@ sed -i -e "s/^APP_URL=http:\/\/localhost/APP_URL=http:\/\/ihub.favbet.dev/g" \
     -e "s/^DB_USERNAME=pgsql/DB_USERNAME=u_ihub/g" \
     -e "s/^DB_PASSWORD=pgsql/DB_PASSWORD=\"b9c3q46-9bv08967\"/g" \
     \
-    -e "s/^REDIS_HOST=example.com/REDIS_HOST=redis-ihub.redis.rancher.internal/g" \
-    -e "s/^REDIS_PORT=6666/REDIS_PORT=6379/g" \
-    -e "s/^REDIS_PREFIX=redis_prefix/REDIS_PREFIX=ihubGrid:ihub-sw/g" \
+    -e "s/^#REDIS_HOST=example.com/REDIS_HOST=redis-ihub.redis.rancher.internal/g" \
+    -e "s/^#REDIS_PORT=6666/REDIS_PORT=6379/g" \
+    -e "s/^#REDIS_PREFIX=redis_prefix/REDIS_PREFIX=ihubGrid:ihub-sw/g" \
     \
-    -e "s/^LOG_RABBIT_HOST=example.com/LOG_RABBIT_HOST=rabbitmq-server.elkr.rancher.internal/g" \
-    -e "s/^LOG_RABBIT_PORT=6666/LOG_RABBIT_PORT=5672/g" \
-    -e "s/^LOG_RABBIT_USER=log_rabbit/LOG_RABBIT_USER=ihub/g" \
-    -e "s/^LOG_RABBIT_PASS=log_rabbit/LOG_RABBIT_PASS=\"ihub\"/g" \
-    -e "s/^LOG_RABBIT_VHOST=null/LOG_RABBIT_VHOST=\"ihub_sw\"/g" \
+    -e "s/^#LOG_RABBIT_HOST=example.com/LOG_RABBIT_HOST=rabbitmq-server.elkr.rancher.internal/g" \
+    -e "s/^#LOG_RABBIT_PORT=6666/LOG_RABBIT_PORT=5672/g" \
+    -e "s/^#LOG_RABBIT_USER=log_rabbit/LOG_RABBIT_USER=ihub/g" \
+    -e "s/^#LOG_RABBIT_PASS=log_rabbit/LOG_RABBIT_PASS=\"ihub\"/g" \
+    -e "s/^#LOG_RABBIT_VHOST=null/LOG_RABBIT_VHOST=\"ihub_sw\"/g" \
     \
     -e "s/^API_ACCOUNT_ROH_HOST=example.com/API_ACCOUNT_ROH_HOST=de2ef01d.dev.favorit/g" \
     -e "s/^API_ACCOUNT_ROH_PORT=6666/API_ACCOUNT_ROH_PORT=10102/g" \
